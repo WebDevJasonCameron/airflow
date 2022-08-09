9517bac60d4be28133fb1bbf2904383fda8fbbea
Jason Cameron <dev.jasoncameron@gmail.com>
2022-08-08 21:31:13 -0500

    comp first dag

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -5,6 +5,7 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 import json
 from pandas import json_normalize
@@ -26,7 +27,15 @@ def _process_user(ti):
     processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
 
 
-    # DAG
+def _store_user():
+    hook = PostgresHook(postgres_conn_id='postgres')
+    hook.copy_expert(
+        sql="Copy users FROM stdin WITH DELIMITER as ','",
+        filename='/tmp/processed_user.csv'
+    )
+
+
+# DAG
 with DAG('user_processing', start_date=datetime(2022, 1, 1), schedule_interval='@daily', catchup=False) as dag:
 
     # Create
@@ -49,13 +58,13 @@ with DAG('user_processing', start_date=datetime(2022, 1, 1), schedule_interval='
     is_api_available = HttpSensor(
         task_id='is_api_available',
         http_conn_id='user_api',
-        endepoint='api/'
+        endpoint='api/'
     )
 
     # Get
     extract_user = SimpleHttpOperator(
         task_id='extract_user',
-        http_con_id='user_api',
+        http_conn_id='user_api',
         endpoint='api/',
         method='GET',
         response_filter=lambda response: json.loads(response.text),
@@ -67,3 +76,11 @@ with DAG('user_processing', start_date=datetime(2022, 1, 1), schedule_interval='
         task_id='process_user',
         python_callable=_process_user
     )
+
+    #
+    store_user = PythonOperator(
+        task_id='store_user',
+        python_callable=_store_user
+    )
+
+    create_table >> is_api_available >> extract_user >> process_user >> store_user
